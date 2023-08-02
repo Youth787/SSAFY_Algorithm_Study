bc3680ba507e82baabb37f678a2e41bf3385716d
yeonmi <wkdus885@naver.com>
2023-08-02 22:52:55 +0900

    solve

---
+++ 01_Sort1/2750_수 정렬하기/정연미/solve.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 수_정렬하기_2750 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 입력받을 숫자의 개수
+		int N = sc.nextInt();
+		
+		//숫자를 담을 배열을 생성한다. 
+		int[] array = new int[N];
+		
+		for(int i =0; i<N; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		//배열을 오름차순으로 정렬시켜주는 메소드 
+		Arrays.sort(array);
+		
+		for(int i : array) {
+			System.out.println(i);
+		}
+		
+		
+	}
+}
