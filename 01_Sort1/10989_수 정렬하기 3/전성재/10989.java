0529f6626cc0b9e23d78eda2b95dcdf762cea85d
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-07 14:25:48 +0900

    Update 10989.java

--- 01_Sort1/10989_수 정렬하기 3/전성재/10989.java
+++ 01_Sort1/10989_수 정렬하기 3/전성재/10989.java
@@ -9,7 +9,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int	N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		
 		for(int i = 0; i < N; i++) {
