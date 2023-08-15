9b870da7dfc557474fcf466fc4b2eb95cb07895e
yeonmi <wkdus885@naver.com>
2023-08-15 21:55:01 +0900

    solve

--- 02_Sort2/18870_좌표 압축/정연미/Main.java
+++ 02_Sort2/18870_좌표 압축/정연미/Main.java
@@ -16,35 +16,35 @@ public class Main{
 		int T = Integer.parseInt(br.readLine()); // 입력받을 숫자의 개수
 
 		int[] array = new int[T]; // 숫자를 입력받을 원본 배열 생성
-		HashSet<Integer> hashset = new HashSet<>();
+		HashSet<Integer> hashset = new HashSet<>(); // 중복없이 받을 hashset 생성 
 
 		StringTokenizer st = new StringTokenizer(br.readLine()); // 띄어쓰기를 기준으로 읽기 위해
 		for (int i = 0; i < T; i++) {
 			int n = Integer.parseInt(st.nextToken());
-			array[i] = n;
-			hashset.add(n);
+			array[i] = n; // 원본 배열로 사용할 배열 하나 입력받고 
+			hashset.add(n); // hashset에도 동일하게 입력받는다. 
 		} // 입력 받음.
 		
 		int i=0;
-		int[] sorted = new int[hashset.size()];
+		int[] sorted = new int[hashset.size()]; // hashset크기의 배열을 하나 생성 
 		for(int n : hashset) {
 			sorted[i++] = n;
-		}
+		} // hashset의 원소를 배열에 넣는다. => hashset을 array로 전환 
 		
-		Arrays.sort(sorted);
+		Arrays.sort(sorted); // 배열을 정렬한다. 
 		
-		Map<Integer, Integer> map_sort = new HashMap<>();
+		Map<Integer, Integer> map_sort = new HashMap<>(); // hashmap 생성한다. 
 
 		int value = 0;
 		for (int key : sorted) {
 			map_sort.put(key, value);
 			value++;
-		}
+		} // 담겨있는 배열 순서대로 0부터 차례로 인덱스를 value로 설정해준다. 
 
         StringBuilder sb = new StringBuilder();
 		for (int n : array) {
 			sb.append(map_sort.get(n) + " ");
-		}System.out.println(sb);
+		}System.out.println(sb); // stringbuilder를 사용해서 출력한다. 
 	}// main end
 }
 
