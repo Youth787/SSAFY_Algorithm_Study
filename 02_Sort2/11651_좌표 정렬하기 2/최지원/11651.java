fc5c37fd94e33090b098a4ee5d36725ad6152d82
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-08-16 20:26:14 +0900

    Update 11651.java

--- 02_Sort2/11651_좌표 정렬하기 2/최지원/11651.java
+++ 02_Sort2/11651_좌표 정렬하기 2/최지원/11651.java
@@ -24,29 +24,61 @@ public class Main {
 		StringTokenizer st; //StringTokenizer 사용
 		
 		for (int i = 0; i < N ; i++) {
+			//StringTokenizer() : 지정된 문자열에 대한 문자열 토크나이저를 구성한다
 			st = new StringTokenizer(br.readLine()); //BufferedReader로 다음 줄을 읽어서 StringTokenizer를 사용하겠다
-			
+
+			//st.nextToken(문자열) : 이 문자열 토크나이저에서 다음 토큰을 반환한다
 			arr[i][0] = Integer.parseInt(st.nextToken()); //arr[i][0]에는 x좌표를
 			arr[i][1] = Integer.parseInt(st.nextToken()); //arr[i][1]에는 y좌표를 저장한다
-		}
+		}  //이걸 N번 반복해서 arr 안에 값을 모두 넣는다
 
-    //크기를 비교해 정렬하는 람다식 활용 
-		Arrays.sort(arr, (e1, e2)->{ //arr에서 e1과 e2를 비교하겠다?
+		//람다식 사용
+    		//람다식 : 메소드를 하나의 식으로 표현한 것. 
+		//람다식 형식 : (매개변수 목록) -> {메소드 몸체} 
+		//+) 즉 Arrays.sort(arr, ~ 뒷부분이 람다식 사용한 부분 
+		Arrays.sort(arr, (e1, e2)->{ //arr에서 e1과 e2를 비교하겠다
 			if (e1[1] == e2[1]) { //y좌표가 같다면 
-				return e1[0]-e2[0]; //x좌표끼리 비교하고
-			} else {
-				return e1[1]-e2[1]; //y좌표가 다르면 둘이 비교하겠다
+				return e1[0]-e2[0]; //x좌표끼리 비교하여 오름차순 정렬 //e2[0]-e1[0]이면 내림차순
+			} else { //그게 아니면 (y좌표가 다르면) 
+				return e1[1]-e2[1]; //y좌표로 오름차순 정렬
 			}
 		});
 
-    //StringBuilder를 사용하여 정렬된 arr의 값들을 더하는 과정
+    		//StringBuilder를 사용하여 정렬된 arr의 값들을 더하는 과정
 		StringBuilder sb = new StringBuilder();
 		for (int i=0; i <N ; i++) {
 			sb.append(arr[i][0]+" "+arr[i][1]).append('\n');
-		}
+		} //정렬된 좌표를 append 사용하여 쭉 이어진 문자열로 만들고
 
 		System.out.println(sb); //출력
 		
 	}
 
 }
+
+//추가내용들
+/* Comparator로 2차원배열 정렬 구현하는 방법
+import java.util.Arrays;
+import java.util.Comparator;
+
+int[][] arrays = { { 0, 3 }, { 2, 6 }, { 1, 9 }, { 1, 8 } };
+
+Arrays.sort(arrays, new Comparator<int[]>() {
+  @Override
+  public int compare(int[] o1, int[] o2) {
+    if (o1[0] == o2[0])
+      return o1[1] - o2[1];
+    else
+      return o1[0] - o2[0];
+  }
+});
+*/
+
+/* 람다식으로 2차원배열 정렬 구현하는 방법
+import java.util.Arrays;
+
+int[][] arrays = { { 0, 3 }, { 2, 6 }, { 1, 9 }, { 1, 8 } };
+Arrays.sort(arrays, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+
+*/
+
