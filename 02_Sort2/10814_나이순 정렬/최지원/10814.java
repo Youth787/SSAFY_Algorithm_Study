757170f1b2c0bc096726de2b4b560e2f29a98cf9
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-08-16 20:32:52 +0900

    Update 10814.java

--- 02_Sort2/10814_나이순 정렬/최지원/10814.java
+++ 02_Sort2/10814_나이순 정렬/최지원/10814.java
@@ -29,11 +29,13 @@ public class Main {
 			ageName[i][0] = st.nextToken(); //나이는 [i][0]번 인덱스에
 			ageName[i][1] = st.nextToken(); //이름은 [i][1]번 인덱스에 저장
 		}
+
 		
+
 		Arrays.sort(ageName, new Comparator<String[]>() { //ageName 이라는 배열에 대해 String[]라는 형을 비교할꺼임
 			public int compare(String[] str1, String[] str2) { //String[]형인 str1과 str2를 비교하는데
 				return Integer.parseInt(str1[0]) - Integer.parseInt(str2[0]); 
-        //형변환한 나이를 비교하겠다. 같으면 어짜피 가입(입력받은) 순 그래도 유지될 것
+       			 //형변환한 나이를 비교하겠다. 같으면 어짜피 가입(입력받은) 순 그래도 유지될 것
 			}
 		});
 		
@@ -41,7 +43,7 @@ public class Main {
 		
 		for (int i = 0 ; i < N ; i++) {
 			sb.append(ageName[i][0]).append(' ').append(ageName[i][1]).append('\n');
-		}
+		} //정렬된 좌표를 append 사용하여 쭉 이어진 문자열로 만들고
 		
 		System.out.println(sb); //출력
 		
@@ -49,3 +51,22 @@ public class Main {
 	}
 
 }
+
+
+//추가내용들
+/* Comparator로 2차원배열 정렬 구현하는 방법
+import java.util.Arrays;
+import java.util.Comparator;
+
+int[][] arrays = { { 0, 3 }, { 2, 6 }, { 1, 9 }, { 1, 8 } };
+
+Arrays.sort(arrays, new Comparator<int[]>() {
+  @Override
+  public int compare(int[] o1, int[] o2) {
+    if (o1[0] == o2[0])
+      return o1[1] - o2[1];
+    else
+      return o1[0] - o2[0];
+  }
+});
+*/
