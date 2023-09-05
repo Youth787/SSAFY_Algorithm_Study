a0adea7a1304b47cc6250188c44ff93bcb241052
aycho00 <141216710+aycho00@users.noreply.github.com>
2023-09-05 22:42:26 +0900

    Add files via upload

---
+++ 05_자료구조1/10845_큐/조아영/Main10845_큐.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main10845_큐 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int commands = Integer.parseInt(br.readLine());
+		int[] queue = new int[commands+1]; 
+		int front = 0; 
+		int back = 0; 
+		StringTokenizer st; 
+		
+		for (int c=1; c<=commands; c++) {
+			st = new StringTokenizer(br.readLine()," "); 
+			switch (st.nextToken()) {
+			case "push":
+				queue[++back]=Integer.parseInt(st.nextToken());
+				break;
+			case "pop":
+				if (front!=back) {
+					System.out.println(queue[front+1]);
+					queue[front+1]=0;
+					front++;
+				}
+				else System.out.println(-1);
+				break;
+			case "size":
+				if (front!=back) System.out.println(back-front);
+				else System.out.println(0);
+				break;
+			case "empty":
+				if (front!=back) System.out.println(0);
+				else System.out.println(1);
+				break;
+			case "front":
+				if (front!=back) System.out.println(queue[front+1]);
+				else System.out.println(-1);
+				break;
+			case "back":
+				if (front!=back) System.out.println(queue[back]);
+				else System.out.println(-1);
+				break;
+			}
+				
+		}
+			
+
+	}
+	
+}
