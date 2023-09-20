d9e85f21a76afab9b8316aabf25ad25d353de2ad
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-09-21 00:53:06 +0900

    Update 21278.java

--- 10_완전 탐색/21278_호석이 두마리 치킨/전성재/21278.java
+++ 10_완전 탐색/21278_호석이 두마리 치킨/전성재/21278.java
@@ -1,86 +1,101 @@
-// 호석이 네 이놈!!!!!!!!!!!!!!!!!!!!!!
-
-
+// 플로이드 와샬 알고리즘 = a->c로 가는 값보다 b를 거쳐 a->b + b->c 값이 더 작다면 작은 값을 택하는 알고리즘.
+//바로 위의 로직은 하나의 노드를 거쳐 가지만(a->b->c),
+//해당 로직이 반복되면 수많은 노드를 거쳐도 (a->b->c->d->e)
+//시작노드와 도착노드만 남게 된다 (a->e)
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_B_G5_21278_호석이두마리치킨_정세린 {
-	static final int INF = 1000000;
-	static int[][] dist;
+public class Main {
+	static int[][] floyd;
 	static int N, M;
-	static int[] selected = new int[2];
-	static int minDist = Integer.MAX_VALUE;
-	static int minL, minS; // 최소 거리를 갖는 건물 L(큰수), S(작은수)
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken()); // 건물의 개수 (1 ~ N번)
-		M = Integer.parseInt(st.nextToken()); // 도로의 개수
-		dist = new int[N + 1][N + 1]; 
-		for (int i = 0; i <= N; i++) Arrays.fill(dist[i], INF);
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		floyd = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				floyd[i][j] = 101; // 점의 최대 개수는 100개이므로, 점과 점은 아무리 멀어도 거리 99가 최대임.
+				// 즉, 거리 101로 해주는 것은 다익스트라의 Integer.MAX_VALUE와 같음.
+			}
+		}
+		// 존재하는 길이 없다. 를 101 로 표현.
+		// 있으면, 거리는 전부 1이니 해당 값 넣기
 
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			dist[a][b] = 1;
-			dist[b][a] = 1;
-		} // end of input
-		
-		// 1. FloydWarshall
-		for (int k = 1; k <= N; k++) { // 거쳐가는 노드
-			for (int i = 1; i <= N; i++) { // 출발노드
-				for (int j = 1; j <= N; j++) { // 도착노드
-					if (dist[i][k] + dist[k][j] < dist[i][j]) 
-						dist[i][j] = dist[i][k] + dist[k][j];
+			floyd[a - 1][b - 1] = 1;
+			floyd[b - 1][a - 1] = 1;
+		}
+		// 초깃값.
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i == j)
+					continue;
+				for (int k = 0; k < N; k++) {
+					if (i == k || j == k)
+						continue;
+
+					if (floyd[j][k] > floyd[j][i] + floyd[i][k])
+						floyd[j][k] = floyd[j][i] + floyd[i][k];
+
 				}
 			}
 		}
-		// 2. 조합
-		combination(1, N, 2);
-		
-		System.out.println(minS + " " + minL + " " + minDist);
-	} // end of main
 
-	static void combination (int idx, int n, int r) {
-		if (r == 0) { 
-			// 3. 최소거리 구하기
-			getMinDist();
-			return;
-		}
-		
-		for (int i = idx; i <= n; i++) {
-			selected[r - 1] = i;
-			combination(i + 1, n, r - 1);
-		}
-		
-	} // end of combination
-	
-	static void getMinDist() {
-		int sum = 0;
-		for (int i = 1; i <= N; i++) { // 건물 i에서 치킨집으로 가는데걸리는 시간
-			if (i == selected[0] || i == selected[1]) continue; // 치킨건물이면 패스
-			int distA = dist[i][selected[0]];
-			int distB = dist[i][selected[1]];
-			sum = sum + Integer.min(distA, distB) * 2; // 왕복거리
-		}
-		if (sum < minDist) {
-			minDist = sum;
-			minL = selected[0]; // 큰수
-			minS = selected[1]; // 작은수
-		} else if (sum == minDist) { // 최소거리합이 같으면
-			if ((minS > selected[1]) || // 작은수가 작은것을 선택
-				(minS == selected[1] && minL > selected[0])) { // 작은수가 같으면 큰수가 큰것을 선택
-				minDist = sum;
-				minL = selected[0]; // 큰수
-				minS = selected[1]; // 작은수
+		// 플로이드 워셜로 각 점에서 각 점까지의 거리의 최솟값 다 채운 후
+		// 플로이드 워셜 로직 = a->c로 가는 값보다 b를 거쳐 a->b + b->c 값이 더 작다면 작은 값을 택하는 알고리즘.
+
+		int min = Integer.MAX_VALUE;
+		int n1 = 0;
+		int n2 = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i == j)
+					continue;
+				// 두 점을 고른다. (1, 2부터 N-1, N까지)
+
+				int now = sum(i, j);
+				// 고른 두 점을 기준으로 각각의 점의 거리의 최솟값을 더해나간다.
+				// 1, 2를 골랐으면
+				// sum(1, 2) = 그 둘(1, 2)에서 3까지의 거리의 최솟값 + ... + 그 둘(1, 2)에서 N 까지의 거리의 최솟값
+
+				if (min > now) {
+					n1 = i + 1;
+					n2 = j + 1;
+					min = now;
+				}
+				// 해당 값이 최솟값이 된다면 두 점을 기록해놓는다.
+
 			}
 		}
-	} 
-	
+
+		// 기록한 두 점과 최솟값을 출력한다.
+		System.out.printf("%d %d %d", n1, n2, min * 2); // min*2는 왕복이라서.
+
+	}
+
+	static int sum(int n1, int n2) {
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			if (i == n1 || i == n2)
+				continue;
+			sum += Math.min(floyd[n1][i], floyd[n2][i]);
+		}
+
+		return sum;
+	}
+
 }
