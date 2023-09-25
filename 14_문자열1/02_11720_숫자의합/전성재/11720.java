d0ff9545688aed18e0930f5d1d6604d8ca6e73de
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-09-25 17:45:31 +0900

    Update and rename test.txt to 11720.java

---
+++ 14_문자열1/02_11720_숫자의합/전성재/11720.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += s.charAt(i) - '0';
+		}
+		System.out.println(sum);
+	}
+
+}
