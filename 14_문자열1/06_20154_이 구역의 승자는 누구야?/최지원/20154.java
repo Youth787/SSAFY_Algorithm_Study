3f254546bf9d01be26a0cb8a3e6a8dd0c126416c
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-09-26 15:34:13 +0900

    Update 20154.java

--- 14_문자열1/06_20154_이 구역의 승자는 누구야?/최지원/20154.java
+++ 14_문자열1/06_20154_이 구역의 승자는 누구야?/최지원/20154.java
@@ -11,12 +11,20 @@ public class Main {
 		//알파벳 대문자로만 이루어진 길이 k인 문자열 s
 		String str = sc.nextLine();
 		int k = str.length();
-		int [] kk = new int[k];
+		int [] kk = new int[k];//주어진 문자열의 각 획을 저장하는 배열
 		for (int i = 0; i < k; i++) {
-			kk[i] = str.charAt(i) - 'A';
+			kk[i] = write[str.charAt(i) - 'A']; //문자열의 i번째 글자가 write의 어디에 해당하는지를 찾고, kk에 저장하는 과정
 		}
+
+		int sum = 0;
 		
-		System.out.println("I'm a winner!");
-		System.out.println("You're the winner?");
+		for (int i = 0; i < kk.length; i++){
+			sum += kk[i];
+			sum %= 10; //10이 넘어가면 10으로 나눈 나머지를 저장
+		}
+
+		if (sum % 2 == 0) System.out.println("You're the winner?");
+		else  System.out.println("I'm a winner!");
+		
 	}//main
 }//class
