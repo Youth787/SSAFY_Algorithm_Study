94831ff975b5d927f28d283b5e961892973bfe62
aycho00 <141216710+aycho00@users.noreply.github.com>
2023-09-26 16:49:05 +0900

    Update Main1316_그룹단어체커.java

--- 14_문자열1/07_1316_그룹 단어 체커/조아영/Main1316_그룹단어체커.java
+++ 14_문자열1/07_1316_그룹 단어 체커/조아영/Main1316_그룹단어체커.java
@@ -1,37 +1,32 @@
 import java.util.Scanner;
 
+//참고: https://propercoding.tistory.com/166
 public class Main1316_그룹단어체커 {
+
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		String[] words = new String[n];
+		int n = sc.nextInt();//단어 개수
+		int ans = n; //일단 다 정답이라고 시작, 만약 정답 아닌 거 발견하면 -1씩 할 것 
 		for (int i=0; i<n; i++) {
-			words[i]=sc.next();
-		}
-		
-		String apb = "abcdefghijklmnopqrstuvwxyz";
-		char[] apbarr= apb.toCharArray();
-		
-		int ans = n; 
-		
-		for (int i=0; i<n; i++) {
-			String str = words[i];
-			boolean test[] = new boolean[26];
-			
-			for (int j=0; j<str.length(); j++) {
-				if (str.charAt(j)!=str.charAt(i+1)) {
-					for (int k=0; k<26; k++) {
-						if (test[k]==true) {
-							ans--;
-							break;
-						}
+			String str = sc.next();//단어 입력 받아 
+			boolean[] arr = new boolean[26];//이미 쓴 알파벳인지 체크용 
+			arr[str.charAt(0)-'a'] = true; //단어의 첫글자 체크 
+			//System.out.println(str.charAt(0)-'a'); //첫글자 잘 뽑았는지 확인용 
+			for (int j=1; j<str.length(); j++) {
+				char ch = str.charAt(j);
+				if (ch==str.charAt(j-1)) continue; //앞글자와 같으면 넘어가 
+				else {
+					if (arr[ch-'a']) {//앞글자와 다른 글자 나왔는데 그게 이미 쓴 거면 그룹단어아님 
+						ans--; 
+						break;
 					}
-					test[str.charAt(j)]=true;
+					arr[ch-'a'] = true; //새로운 글자 나왔으니 이전 글자는 썼다고 표시  
 				}
 			}
-			System.out.println(ans);
 		}
-		
+		System.out.println(ans);
+
 	}
+	
 }
