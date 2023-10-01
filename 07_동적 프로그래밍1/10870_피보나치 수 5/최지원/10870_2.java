687a3ccef8326e3aac114e46cf11749871fb88ae
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-10-01 22:40:55 +0900

    Update 10870_2.java

--- 07_동적 프로그래밍1/10870_피보나치 수 5/최지원/10870_2.java
+++ 07_동적 프로그래밍1/10870_피보나치 수 5/최지원/10870_2.java
@@ -16,3 +16,29 @@ public class Main {
 		return fibo(a-1) + fibo(a-2);
 	}
 }//class
+
+
+/*
+import java.util.Scanner;
+
+public class Main {
+	public static int [] dp; 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		dp = new int [n+1];
+		System.out.println(fibo(n));
+	}//main
+	
+	public static int fibo(int a) {
+		//dp[0] = 0;
+		dp[1] = 1;
+		
+		for (int i = 2 ; i <= a; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		return dp[a];
+	}
+}//class
+
+*/
