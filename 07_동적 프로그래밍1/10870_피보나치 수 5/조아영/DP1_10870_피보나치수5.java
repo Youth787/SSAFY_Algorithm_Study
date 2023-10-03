83a5747e87417438b2579b41f920bfe4ea586b93
aycho00 <141216710+aycho00@users.noreply.github.com>
2023-10-03 16:37:59 +0900

    Update DP1_10870_피보나치수5.java

--- 07_동적 프로그래밍1/10870_피보나치 수 5/조아영/DP1_10870_피보나치수5.java
+++ 07_동적 프로그래밍1/10870_피보나치 수 5/조아영/DP1_10870_피보나치수5.java
@@ -8,7 +8,7 @@ public class DP1_10870_피보나치수5 {
 		int n = sc.nextInt();
 		int[] dp = new int[n+1];
 		dp[0] = 0;
-		dp[1] = 1; 
+		if (n>0) dp[1] = 1; 
 		
 		for (int i=2; i<=n; i++) {
 			dp[i] = dp[i-1]+dp[i-2];			
