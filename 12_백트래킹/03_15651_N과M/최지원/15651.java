455f5a5231821a4000179757342e0c9bff9132bd
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-10-05 20:24:38 +0900

    Update and rename test.txt to 15651.java

---
+++ 12_백트래킹/03_15651_N과M/최지원/15651.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static int[] arr;
+	public static int n, m;
+	public static StringBuilder sb = new StringBuilder();
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+ 
+		n = sc.nextInt();
+		m = sc.nextInt();
+ 
+		arr = new int[m];
+        
+		DFS(0);
+		System.out.print(sb);
+ 
+	}
+ 
+	public static void DFS(int depth) {
+		//기저조건 m에 도달 ?: 출력후 return
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.append('\n');
+			return;
+		}
+ 
+		for (int i = 1; i <= n; i++) {
+			arr[depth] = i;
+			DFS(depth + 1);
+		}
+	}
+ 
+}
