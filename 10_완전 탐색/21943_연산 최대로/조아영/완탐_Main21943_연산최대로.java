49d6021eb81efd66e273d81f7663373e634b1087
aycho00 <141216710+aycho00@users.noreply.github.com>
2023-10-05 17:53:38 +0900

    Update and rename test.txt to 완탐_Main21943_연산최대로.java

---
+++ 10_완전 탐색/21943_연산 최대로/조아영/완탐_Main21943_연산최대로.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//https://codingrapper.tistory.com/201
+//https://record-developer.tistory.com/164
+//https://welog.tistory.com/331
+public class 완탐_Main21943_연산최대로 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] nums = new int[n];
+		for (int i=0; i<n; i++) {
+			nums[i] = sc.nextInt(); //양의 정수들 
+		}
+		int p = sc.nextInt(); //더하기 기호 개수
+		int q = sc.nextInt(); //곱하기 기호 개수
+		
+		//가능한 연산의 결과 중 최댓값을 출력해야 해 
+		
+		
+		
+	}
+	
+	
+	
+	
+}
