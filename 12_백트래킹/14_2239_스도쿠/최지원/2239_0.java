b196edc4c79bf47071dc69d928c5fc62f8a6fc69
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-10-12 09:00:08 +0900

    Update 2239_0.java

--- 12_백트래킹/14_2239_스도쿠/최지원/2239_0.java
+++ 12_백트래킹/14_2239_스도쿠/최지원/2239_0.java
@@ -22,10 +22,10 @@ public class Main {
 					map[i][j] = input.charAt(j) - '0';
 				}
 			}
-//입력
+			//입력
 			
 			dfs(0);
-//빈 부분 작성
+			//빈 부분 작성
 			
 			for (int i = 0; i < 9; ++i) {
 				for (int j = 0; j < 9; ++j) {
@@ -36,30 +36,30 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-//작성한 스도쿠 출력
+	//작성한 스도쿠 출력
 	
 
-//0으로 된 칸 채우는 dfs. 인자는 지금 탐색하는 칸의 번호(depth)
+	//0으로 된 칸 채우는 dfs. 인자는 지금 탐색하는 칸의 번호(depth)
 	public static void dfs(int depth) {
-//기저 : 모든 칸을 다 돌았을 때(depth가 0~8까지 돌고 81이 되었을때
+	//기저 : 모든 칸을 다 돌았을 때(depth가 0~8까지 돌고 81이 되었을때
 		if (depth == 81) {
 			end = true;//스도쿠 채우기가 끝났다
 			return;//리턴
 		}
 		
-//내부에서 사용할 변수를 인자를 통해 만든다
+		//내부에서 사용할 변수를 인자를 통해 만든다
 		int y = depth / 9;//depth를 9로 나눴을때 몫은 현재 칸이 몇번째 행에 있는지 (0~8)
 		int x = depth % 9;//나머지는 현재칸이 몇번째 열에 있는지 (0~8)
 		
 		if (map[y][x] != 0) {
-//칸이 이미 채워져있으면 다음칸으로 넘어가
+			//칸이 이미 채워져있으면 다음칸으로 넘어가
 			dfs(depth + 1);
 		} else {
-//값이 0이면 아직 미작성한 칸이므로 for문을 통해 1~9 숫자를 넣어보겠다
+			//값이 0이면 아직 미작성한 칸이므로 for문을 통해 1~9 숫자를 넣어보겠다
 			for (int i = 1; i <= 9; ++i) {
-//map[y][x]에 i가 가능한지 isValid에서 확인하여
+				//map[y][x]에 i가 가능한지 isValid에서 확인하여
 				if (!isVaild(y, x, i)) continue;//불가능하면 숫자 i는 넘어가, 다음 숫자 넣어본다
-//숫자 i를 넣는게 가능하다면
+				//숫자 i를 넣는게 가능하다면
 				map[y][x] = i;//넣고
 				dfs(depth + 1);//다음칸으로 넘어간다
 				if (end) return;//기저조건에 있는 end가 true상태로 리턴되었다면, 여기 if문에서도 리턴=어디로 나가지???
@@ -68,14 +68,14 @@ public class Main {
 		}
 	}
 	
-//map[y][x]에 n을 넣을 수 있는지 확인
+	//map[y][x]에 n을 넣을 수 있는지 확인
 	public static boolean isVaild(int y, int x, int n) {
-//가로, 세로 줄 에 같은 숫자가 있는지 확인
+		//가로, 세로 줄 에 같은 숫자가 있는지 확인
 		for (int i = 0; i < 9; ++i) {
 			if (map[y][i] == n || map[i][x] == n) return false;
 		}
 		
-//3 * 3 칸안에 같은 숫자가 있는지 확인 
+		//3 * 3 칸안에 같은 숫자가 있는지 확인 
 		int yy = y / 3 * 3;
 		int xx = x / 3 * 3;
 		for (int i = yy; i < yy + 3; ++i) {
@@ -95,7 +95,7 @@ public class Main {
 
 
 //-----------------------------------------------------------------------------------------------
-//풀이 3 : bit 연산을 사용 https://nahwasa.com/entry/%EB%B0%B1%EC%A4%80-2239-%EC%9E%90%EB%B0%94-%EC%8A%A4%EB%8F%84%EC%BF%A0-BOJ-2239-JAVA
+//풀이 3 : bit 연산을 사용 ( https://nahwasa.com/entry/%EB%B0%B1%EC%A4%80-2239-%EC%9E%90%EB%B0%94-%EC%8A%A4%EB%8F%84%EC%BF%A0-BOJ-2239-JAVA )
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
