fba74e30b561305ddd6150a0e14af96a16929c21
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-10-17 16:09:25 +0900

    Update and rename test.txt to 12865.java

---
+++ 08_동적 프로그래밍2/12865_평범한 배낭/전성재/12865.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[][] item = new int[N + 1][2];  // weight, value
+
+        for (int i = 1; i <= N; i++) {
+        	st = new StringTokenizer(br.readLine());
+            item[i][0] = Integer.parseInt(st.nextToken());
+            item[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int k = 1; k <= K; k++) { // 무게
+            for (int i = 1; i <= N; i++) { // item
+                dp[i][k] = dp[i - 1][k];
+                if (k - item[i][0] >= 0) {
+                    dp[i][k] = Math.max(dp[i - 1][k], item[i][1] + dp[i - 1][k - item[i][0]]);
+                }
+            }
+        }
+
+
+        System.out.println(dp[N][K]);
+    }
+}
