4b19a51ad0bec039b18b273989f58492241d2f95
aycho00 <141216710+aycho00@users.noreply.github.com>
2023-10-25 22:58:15 +0900

    Update and rename test.txt to Main1074_Z.java

---
+++ 13_분할정복/06_1074_Z/조아영/Main1074_Z.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main1074_Z {
+	
+    static int n;
+    static long m, max;
+    static int [] arr;
+    static long result = Long.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		 String[] s = br.readLine().split(" ");
+	        n = Integer.parseInt(s[0]);
+	        m = Integer.parseInt(s[1]);
+	 
+	        arr = new int[n];
+	        for(int i=0; i<n; i++){
+	            arr[i] = Integer.parseInt(br.readLine());
+	            max = Math.max(max,arr[i]);
+	        }
+	        Arrays.sort(arr);
+	 
+	        binarySearch();
+	 
+	        System.out.println(result);
+	}
+	 
+    static void binarySearch(){
+        long low = 0;
+        long high = m * max;
+ 
+        while(low<=high){
+            long mid = (low+high)/2;
+            long sum = 0;
+            for(long index: arr){
+                long count = mid/index;
+ 
+                if(sum>=m) break;
+                sum+=count;
+            }
+            if(sum>=m){
+                high = mid-1;
+                result = Math.min(mid,result);
+            } 
+            else low = mid+1;
+
+        }
+
+	}
+	
+}
