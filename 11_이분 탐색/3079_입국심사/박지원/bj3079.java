46b6dce56fdbedeb928dbd05e1b2c4a2f1f34878
zziony <122343467+zzionyy@users.noreply.github.com>
2023-10-26 14:36:25 +0900

    Update bj3079.java

--- 11_이분 탐색/3079_입국심사/박지원/bj3079.java
+++ 11_이분 탐색/3079_입국심사/박지원/bj3079.java
@@ -21,7 +21,7 @@ public class bj3079 {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr); // 이분탐색은 정렬이 필수!
+        Arrays.sort(arr);
         max = arr[n - 1]; // 시간의 최대값 구하려고 변수 설정
 
         solve();
@@ -55,6 +55,7 @@ public class bj3079 {
 // 이진 탐색 -> 시간을 중심으로 탐색
 // 해당 시간일 때 최대 몇 명의 사람을 입국 심사대에서 통과 시킬 수 있는가를 중점으로 이진탐색을 진행
 // 만약 mid초일 때, m명 이상의 사람을 해당 시간에 입국 심사대에서 보낼 수 있음 -> 시간 탐색범위를 0 ~ mid-1
-// 만약 mid초 일 때, m-1 이하의 사람을 해당 시간에 입국 심사대에서 보낼 수 있음 -> 시간 탐색 범위를 mid+1 ~right
-// 처음 left는 0으로 설정, right는 각 입국심사대에서 걸리는 시간의 최대값(Tk) * 심사 받으려는 사람의 수(M)
+// 만약 mid초 일 때, m-1 이하의 사람을 해당 시간에 입국 심사대에서 보낼 수 있음 -> 시간 탐색 범위를 mid+1 ~ high
+// 처음 low는 0으로 설정, high는 각 입국심사대에서 걸리는 시간의 최대값(t) * 심사 받으려는 사람의 수(m)
+// https://developer-ellen.tistory.com/98
 // https://dingdingmin-back-end-developer.tistory.com/entry/%EB%B0%B1%EC%A4%80-3079-%EC%9E%90%EB%B0%94-java-%EC%9E%85%EA%B5%AD-%EC%8B%AC%EC%82%AC
