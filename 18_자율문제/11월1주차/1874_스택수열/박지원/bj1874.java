c4c97947fcd8dee874d6c032147a0d7a86f5d261
zziony <122343467+zzionyy@users.noreply.github.com>
2023-11-01 08:21:15 +0900

    Update bj1874.java

--- 18_자율문제/11월1주차/1874_스택수열/박지원/bj1874.java
+++ 18_자율문제/11월1주차/1874_스택수열/박지원/bj1874.java
@@ -10,14 +10,14 @@ public class bj1874 {
         StringBuilder sb = new StringBuilder();
         int start = 0;
         Stack<Integer> s = new Stack<>();
-        while (n-- > 0) {
-        	int now = Integer.parseInt(br.readLine());
-        	if (now > start) {
+        while (n-- > 0) { // n이 0보다 클때 돌아돌아
+        	int now = Integer.parseInt(br.readLine()); // 숫자 받아오고
+        	if (now > start) { // 만약 내 현재 값(start)보다 크다면 숫자를 스택에 넣어주고 + 출력
         		for (int i = start + 1; i <= now; i++) {
         			s.push(i);
         			sb.append("+").append("\n");
         		}
-        		start = now;
+        		start = now; // 현재 값 초기화!!!!
         	} else if (s.peek() != now) { // top에 있는 원소가 입력받은 값과 다른 경우는 no
         		System.out.println("NO");
         		return;
