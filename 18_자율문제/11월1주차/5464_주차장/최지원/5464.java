35d40d43d172d4c18a47a48717c68cba2d5b5bc8
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-11-02 21:39:11 +0900

    Update 5464.java

--- 18_자율문제/11월1주차/5464_주차장/최지원/5464.java
+++ 18_자율문제/11월1주차/5464_주차장/최지원/5464.java
@@ -1,10 +1,12 @@
-//푸는 중
+//52번줄 인덱스 예외엥
 package p5464_주차장;
 // https://www.acmicpc.net/problem/5464
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 //문제: 주차 공간별 요금과 차량들의 무게와 출입 순서가 주어질 때, 오늘 하룻동안 주차장이 벌어들일 총 수입을 계산
@@ -17,7 +19,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int n, m;//주차공간 수 n, 차량 수 m
 	static boolean [] visitedN, visitedM;//
-	static int [] Rs, Wk;//주차 공간 s의 단위 무게당 요금, 차량 k의 무세 저장하는 배열
+	static int [] Rs, Wk, parking ;//주차 공간 s의 단위 무게당 요금, 차량 k의 무게 저장하는 배열, 차가 주차된 위치
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,7 +31,36 @@ public class Main {
 		Wk = new int [m+1];//차 번호는 1번부터니까
 		for (int i = 1; i <= m; i++) Wk[i] = Integer.parseInt(br.readLine());
 		
-		//음수면 나가는 경우, 양수면 들어오는 경우
+		int parking[] = new int[m + 1];//주차 위치
+		PriorityQueue<Integer> queue = new PriorityQueue<>();//주차장 큐
+		Queue<Integer> wait = new LinkedList<>();//대기 큐
+
+		for (int i = 1; i <= n; i++) {
+			queue.add(i);
+		}//자리를 다 넣어놓고, 뺐다가 다시 넣는 방식으로 하는거
+
+		int ans = 0;
+
+		//2*M 개의 줄에서 m개의 차량은 정확하게 한번씩 주차장에 출입함.
+		for (int i = 0; i < 2 * m; i++) {
+			int car = Integer.parseInt(br.readLine());//차번호 입력받고
+			if (car > 0 && !queue.isEmpty()) parking[car] = queue.poll();////남은 주차공간 값 하나 빼내고, 그 차가 어디 주차했는지 저장함
+			else if (car > 0 && queue.isEmpty()) wait.add(car);////대기줄에 차를 넣는다
+			else {
+				//음수면 돈 계산. 차가 주차된 공간이 어디인지
+				car = Math.abs(car);
+				ans += Wk[car] * Rs[parking[car]];
+
+				if (!wait.isEmpty()) {
+					//만약 대기하는 차가 있으면: 대기줄 큐에서 차 한대 꺼내서 그 자리에 넣는다
+					parking[wait.poll()] = parking[car];
+				} else {
+					//대기줄에 차가 없다면 그 공간 다시 비워둔다(주차공간 번호를 다시 queue에 넣겠다)
+					queue.add(parking[car]);
+				}
+			}
+		}
+		System.out.println(ans);
 		
 		
 	}//main
