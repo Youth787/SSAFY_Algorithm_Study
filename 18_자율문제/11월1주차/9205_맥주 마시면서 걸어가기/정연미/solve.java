909b50a31b1bd496d527ea450568a3a71e188477
yeonmi <wkdus885@naver.com>
2023-11-04 17:51:51 +0900

    add

--- 18_자율문제/11월1주차/9205_맥주 마시면서 걸어가기/정연미/solve.java
+++ 18_자율문제/11월1주차/9205_맥주 마시면서 걸어가기/정연미/solve.java
@@ -3,34 +3,67 @@ package _11월1주차문제;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class 맥주마시면서걸어가기_9205 {
-	static int[][] arr;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc =1; tc<=T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			arr = new int[N+2][2];
-			for(int i=0; i<N+2; i++) {
-				String[] input = br.readLine().split(" ");
-				arr[i][0] = Integer.parseInt(input[0]);
-				arr[i][1] = Integer.parseInt(input[1]);
-			}//입력받기 
-			
-			int check =0;
-			for(int i=0; i<N+1; i++) {
-				check += 1000 - distance(i,i+1);
-				if(check<0) {
-					System.out.println("sad");
-					return;
-				}
-			}
-			System.out.println("happy");
-		}// test case end 
-	}// main end
-	public static int distance(int x, int y) {
-		return (int) Math.sqrt(Math.pow((arr[x][0] - arr[y][0]) , 2) + Math.pow((arr[x][1] - arr[y][1]) , 2)); 
-	}
+    static boolean[] visit;
+
+    public static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            Point[] point = new Point[N + 2];
+            visit = new boolean[N + 2];
+
+            for (int i = 0; i < N + 2; i++) {
+                String[] input = br.readLine().split(" ");
+                point[i] = new Point(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+            }
+
+            BFS(point);
+        } // test case end
+    }// main end
+
+    public static void BFS(Point[] point) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(point[0]);
+        visit[0] = true;
+        int size = point.length -1;
+        while (!queue.isEmpty()) {
+            Point curr = queue.poll();
+            int x = curr.x;
+            int y = curr.y;
+            if(Math.abs(point[size].x-x)+Math.abs(point[size].y-y)<=1000){
+                System.out.println("happy");
+                return;
+            }
+
+            for(int i=1; i<size; i++){
+                if(!visit[i]){
+                    if(Math.abs(point[i].x-x)+Math.abs(point[i].y-y)<=1000){
+                        visit[i] = true;
+                        queue.offer(point[i]);
+                    }
+                }
+            }
+        }
+        System.out.println("sad");
+        return;
+    }// method end
+
 }
---
+++ 18_자율문제/11월2주차/README.md
@@ -0,0 +1,58 @@
+## 📚 자율 문제
+
+순서대로 푸는게 좋아요.
+
+추천 문제는 (✅ 체크표시) 했습니다.
+
+<br/>
+<table>
+  <tr>
+    <td align="center">순서</td>
+    <td align="center">추천 문제</td>
+    <td align="center">난이도</td>
+    <td align="center">문제 번호</td>
+    <td align="center">문제 이름</td>
+    <td align="center">문제 링크</td>
+  </tr>
+  <tr>
+    <td align="center">01</td>
+    <td align="center">✅</td>
+    <td align="center"><img height="23px" width="25px" src="https://d2gd6pc034wcta.cloudfront.net/tier/9.svg"></td>
+    <td align="center">1260</td>
+    <td align="center">DFS와BFS</td>
+    <td align="center"><a href="https://www.acmicpc.net/problem/1260">바로가기</a></td>
+  </tr>
+  <tr>
+    <td align="center">02</td>
+    <td align="center">✅</td>
+    <td align="center"><img height="23px" width="25px" src="https://d2gd6pc034wcta.cloudfront.net/tier/10.svg"></td>
+    <td align="center">1697</td>
+    <td align="center">숨바꼭질</td>
+    <td align="center"><a href="https://www.acmicpc.net/problem/1697">바로가기</a></td>
+  </tr>
+  <tr>
+    <td align="center">03</td>
+    <td align="center">✅</td>
+    <td align="center"><img height="23px" width="25px" src="https://d2gd6pc034wcta.cloudfront.net/tier/10.svg"></td>
+    <td align="center">2302</td>
+    <td align="center">극장좌석</td>
+    <td align="center"><a href="https://www.acmicpc.net/problem/2302">바로가기</a></td>
+  </tr>
+  <tr>
+    <td align="center">04</td>
+    <td align="center">✅</td>
+    <td align="center"><img height="23px" width="25px" src="https://d2gd6pc034wcta.cloudfront.net/tier/10.svg"></td>
+    <td align="center">20923</td>
+    <td align="center">숫자 할리갈리 게임</td>
+    <td align="center"><a href="https://www.acmicpc.net/problem/20923">바로가기</a></td>
+  </tr>
+  <tr>
+    <td align="center">05</td>
+    <td align="center">✅</td>
+    <td align="center"><img height="23px" width="25px" src="https://d2gd6pc034wcta.cloudfront.net/tier/11.svg"></td>
+    <td align="center">9251</td>
+    <td align="center">LCS</td>
+    <td align="center"><a href="https://www.acmicpc.net/problem/9251">바로가기</a></td>
+  </tr>
+</table>
+<br/><br/>
