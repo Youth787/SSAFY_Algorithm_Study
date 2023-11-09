651938ea989bc3142d36c4fd79f4c2f6256d8f35
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-11-09 09:21:51 +0900

    Update 9251.java

--- 18_자율문제/11월2주차/9251_LCS/전성재/9251.java
+++ 18_자율문제/11월2주차/9251_LCS/전성재/9251.java
@@ -1,5 +1,7 @@
 [![1.png](https://i.postimg.cc/yxfHVWKS/1.png)](https://postimg.cc/p5n7CWp2)
 
+	  https://postimg.cc/p5n7CWp2
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
