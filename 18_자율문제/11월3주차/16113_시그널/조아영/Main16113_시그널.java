70144d6cbcbb2e516e3a39f5c257865b2bf5fe04
aycho00 <141216710+aycho00@users.noreply.github.com>
2023-11-15 14:49:09 +0900

    Update Main16113_시그널.java

--- 18_자율문제/11월3주차/16113_시그널/조아영/Main16113_시그널.java
+++ 18_자율문제/11월3주차/16113_시그널/조아영/Main16113_시그널.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.*;
 
+//메모리초과 ㅜㅜ 
 public class Main16113_시그널 {
 
 	static int n, m;
 	static char[][] arr; 
-	static String ans;
+	static StringBuilder sb = new StringBuilder();
+	static String[][] numbers = new String[10][1];
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -22,31 +24,54 @@ public class Main16113_시그널 {
 			for (int j=0; j<m; j++) {
 				arr[i][j] = st.charAt(cnt++);
 			}
-			System.out.println(arr[i]);
+//			System.out.println(arr[i]);
 		}
 		
-		findNum(0,0); //배열의 0,0부터 볼거얌 
+		numbers[0][0] = "####.##.##.####";
+		numbers[1][0] = "###############"; 
+		numbers[2][0] = "###..#####..###";
+		numbers[3][0] = "###..####..####";
+		numbers[4][0] = "#.##.####..#..#";
+		numbers[5][0] = "####..###..####";
+		numbers[6][0] = "####..####.####";
+		numbers[7][0] = "###..#..#..#..#";
+		numbers[8][0] = "####.#####.####";
+		numbers[9][0] = "####.####..####";
 		
-		System.out.println(ans);
+		find(0,0); //배열의 0,0부터 볼거얌 
+		
+		System.out.println(sb.toString());
 	
 	}
 	
-	static char[][] number = new char[3][5];
-	
-	static char[][] numberOne = {{'#'},{'#'},{'#'},{'#'},{'#'}};
-	
-	static void findNum (int r, int c) {
+	static void find (int r, int c) {
 		
 		if (c==m) return; //만약 배열 넘어가면 리턴해야 됨 
-		else if (arr[r][c]=='.') findNum(r, c+1); //해당 열의 첫칸이 공백이면 다음 열로 넘어가 
+		else if (arr[r][c]=='.') find(r, c+1); //해당 열의 첫칸이 공백이면 다음 열로 넘어가 
 		else { //공백이 아니면(검은색이면) 
 			if (arr[r][c+1]=='.') { //그 다음열 첫칸이 공백이면 숫자 1임 
-				ans += '1';
+				sb.append("1");
 			} else { //다음열 첫칸이 공백 아니면 나머지 숫자 중 뭔지 찾아야 돼 
-				//구현 못함... 
+				int findNum = 10;
+				for (int k=0; k<10; k++) {
+					int idx = 0;
+					if (findNum<10) break; //숫자 찾았으면 더 안해 
+					outer: for (int i=r; i<r+5; i++) {
+						for (int j=c; j<c+3; j++) {
+							if (arr[i][j]!=numbers[k][0].charAt(idx++)) {
+								break outer; //만약 다르면 다음 숫자인지 찾아 
+							}
+							if (i==r+4 && j==c+2) {
+								findNum = k;  //숫자 찾았으면 findNum을 바꿔 
+							}
+						}
+					}
+				}
+				sb.append(findNum); //정답 문자열에 찾은 숫자 넣어줘 
 			}
-		}
-		
-	}
+			find(r,c+3); //그리고 다음 숫자 ㄱㄱ 
+			
+		} //else end 
+	} //find method end 
 	
-}
+} //class end
