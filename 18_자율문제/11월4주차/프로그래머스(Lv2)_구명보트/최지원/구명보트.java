9872aa08e4e8cbd5bab2c3a973d3d90dd6234c1c
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-11-29 21:46:21 +0900

    Update and rename test.txt to 구명보트.java

---
+++ 18_자율문제/11월4주차/프로그래머스(Lv2)_구명보트/최지원/구명보트.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class 구명보트 {
+    public int solution(int[] people, int limit) {      
+        int first = 0;
+//        int last = people.length-1;
+
+        Arrays.sort(people);//먼저 정렬
+
+        int ans = 0;
+        for (int i = people.length -1; i >= first; i--) {
+        	if (people[first] + people[i] <= limit) {
+        		first++;
+        	} 
+        	ans++;
+        }
+        
+//        while (first <= last) {
+//            ans += 1;
+//            if (people[first] + people[last] <= limit) {
+//                first += 1;
+//            }
+//            last -= 1;
+//        }
+        return ans;
+    }
+}
