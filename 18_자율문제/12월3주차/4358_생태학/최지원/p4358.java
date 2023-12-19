dab6ca9d88c7141e78271db2a3721ff03fc4d5b3
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-12-19 22:29:16 +0900

    Update p4358.java

--- 18_자율문제/12월3주차/4358_생태학/최지원/p4358.java
+++ 18_자율문제/12월3주차/4358_생태학/최지원/p4358.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(string[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Map<String,Float> map = new HashMap<>();//나무 종 이름, 나무 개수 저장
