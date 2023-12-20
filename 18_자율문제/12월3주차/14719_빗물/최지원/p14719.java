3145061fefdd9c58adfeed1f2fd45af9556d2a01
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2023-12-20 19:55:42 +0900

    Update p14719.java

--- 18_자율문제/12월3주차/14719_빗물/최지원/p14719.java
+++ 18_자율문제/12월3주차/14719_빗물/최지원/p14719.java
@@ -22,15 +22,21 @@ public class Main {
     private static int count(int[] blocks) {
         int cnt = 0; //고인물 cnt
         
-        //블록마다 계산
+        //블록마다 계산(가장자리 친구들은 물이 고일 수가 없다
         for (int i = 1; i < blocks.length - 1; i++) {
-            int block = blocks[i];
+            int block = blocks[i];//현재 블록 높이
             
-            //현재 블록 기준 왼쪽의 높은 블록, 오른쪽 높은 블록 찾음->둘 중 낮은 곳-현재 높이 차 만큼 cnt+
+            //현재 블록 기준으로 왼쪽, 오른쪽을 구분하여 배열로 복사하고 -> 왼쪽의 높은 블록, 오른쪽 높은 블록 찾음 -> 둘 중 낮은 곳-현재 높이 차 만큼 cnt+
             int[] left = Arrays.copyOfRange(blocks, 0, i);
             int[] right = Arrays.copyOfRange(blocks, i + 1, blocks.length);
+            Arrays.sort(left);
+            Arrays.sort(right);
+            int maxL = left[left.length-1];
+            int maxR = right[right.length-1];
+            /*32~35 줄 이걸로도 사용 가능..배열을 가져와 이를 스트림(Stream)으로 만들고, max()로 OptionalInt를 반환, 다시 int로(getAsInt)
             int maxL = Arrays.stream(left).max().getAsInt();
             int maxR = Arrays.stream(right).max().getAsInt();
+            */
             
             if (block < maxL && block < maxR) {
                 cnt += Math.min(maxL - block, maxR - block);
