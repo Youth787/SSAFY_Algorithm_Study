0a38c0ab7b44021ee6c6b3066a188cecb2277e16
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2024-01-10 20:38:35 +0900

    Update and rename test.txt to 2293.java

---
+++ 18_자율문제/1월2주차/2293_동전1/전성재/2293.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, k;
+    static int[] arr, dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+        dp = new int[k + 1];
+        dp[0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            for (int j = arr[i]; j <= k; j++){
+                dp[j] += dp[j - arr[i]];
+            }
+        }
+        System.out.println(dp[k]);
+
+    }
+}
