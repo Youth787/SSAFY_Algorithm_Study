0e102585f3f6bec70acab37167a66aadbe09a730
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-17 23:57:03 +0900

    Update p11729_1.java

--- 18_자율문제/1월3주차/11729_하노이탑이동순서/최지원/p11729_1.java
+++ 18_자율문제/1월3주차/11729_하노이탑이동순서/최지원/p11729_1.java
@@ -7,12 +7,47 @@ package p11729_하노이탑이동순서;
  * 
  */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
 		
-	} //main
-} //class
+		int count = hanoi(N, 1, 2, 3, 0, sb);
+		
+		bw.write(String.valueOf(count)+"\n");
+		bw.write(sb.toString());
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
+		
+	}
+	
+	public static int hanoi(int n, int start, int sub, int to, int count, StringBuilder sb) {
+		
+		count++;
+		
+		if(n == 1) {
+			sb.append(start + " " + to + "\n");
+			return count;
+		}
+		
+		count = hanoi(n-1, start, to, sub, count, sb);
+		sb.append(start + " " + to + "\n");
+		count = hanoi(n-1, sub, start, to, count, sb);
+		
+		return count;
+	}
+}
