b394b511ebda185d0452d04106f4d65e43c589a0
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-01-17 17:58:40 +0900

    Update and rename test.txt to p11729_1.java

---
+++ 18_자율문제/1월3주차/11729_하노이탑이동순서/최지원/p11729_1.java
@@ -0,0 +1,18 @@
+package p11729_하노이탑이동순서;
+/*
+ * 3개 장대, n개의 원판. 최소 이동횟수 
+ * stack 3개 만들고 이전 값이 
+ * 3번째 장대의 stack size가 n이 되면 종료
+ * 
+ * 
+ */
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+	} //main
+} //class
