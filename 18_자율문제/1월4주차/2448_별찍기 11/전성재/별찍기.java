60ac2335ac348799055938e15170a0fcb48e9321
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2024-01-24 08:35:49 +0900

    Update 별찍기.java

--- 18_자율문제/1월4주차/2448_별찍기 11/전성재/별찍기.java
+++ 18_자율문제/1월4주차/2448_별찍기 11/전성재/별찍기.java
@@ -1,37 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class PrintStar {
-	static String[][] map;
-
-	public static void main(String[] args) throws IOException {
+public class Main {
+	static char[][] arr;
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int m = n * 2 - 1;
-
-		map = new String[n][m];
-		for (int i = 0; i < n; i++) Arrays.fill(map[i], " ");
-
-		partition(0, n - 1, n);
-
-		for (int i = 0; i < n; i++) {
-			System.out.println(Arrays.toString(map[i]).replaceAll(", ", "").substring(1, m + 1));
+		
+		int N = Integer.parseInt(br.readLine());
+		arr = new char[N][(2*N)-1];
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				arr[i][j]=' ';
+			}
 		}
+		
+		//별 채우기
+		//fillStar(첫 행, 가운데 열, 가장 작은 값까지 나누기위한 것(입력받은수)) 
+		fillStar(0,N-1,N);
+		printStar();//출력
 	}
-
-	private static void partition(int row, int col, int n) {
-		if (n == 3) {
-			map[row][col] = "*";
-			map[row + 1][col - 1] = map[row + 1][col + 1] = "*";
-			map[row + 2][col - 2] = map[row + 2][col - 1] = map[row + 2][col] = map[row + 2][col + 1] = map[row + 2][col + 2] = "*";
+	
+	public static void fillStar(int r, int c, int n) {
+		//각 트리가 가장 작은 값으로 됐을때 별찍기
+		if(n==3) {
+			arr[r][c]='*';//트리하나의 맨위의 별
+			arr[r+1][c-1]=arr[r+1][c+1]='*';//가운데 별들
+			arr[r+2][c-2]=arr[r+2][c-1]=arr[r+2][c]=arr[r+2][c+1]=arr[r+2][c+2]='*';//마지막 별들
+			return;//종료
 		}
-		else {
-			int div = n / 2;
-			partition(row, col, div);
-			partition(row + div, col - div, div);
-			partition(row + div, col + div, div);
+		
+		//가장 작은값(가장 작은트리가)이 아닐때 3*2의제곱수 이므로 2로 나눠줌.
+		int size = n/2;
+		fillStar(r, c, size);//모여있는 3개의 트리중 가장위쪽 트리 탐색
+		fillStar(r+size, c-size, size);//3개의 트리중 아래왼쪽 트리 탐색
+		fillStar(r+size, c+size, size);//3개의 트리중 아래오른쪽 트리 탐색
+	}
+	
+	public static void printStar() {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 }
