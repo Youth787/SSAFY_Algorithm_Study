00c653074be43a08328ecaf8a26cfc8c1f14813c
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2024-02-01 19:40:54 +0900

    Update 아기상어.java

--- 18_자율문제/1월4주차/16236_아기상어/전성재/아기상어.java
+++ 18_자율문제/1월4주차/16236_아기상어/전성재/아기상어.java
@@ -1,108 +1,81 @@
-import java.util.*;
-import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
+    static int[] dy = {-1, 0, 0, 1}; // Y좌표 이동 상하좌우
+    static int[] dx = {0, -1, 1, 0}; // X좌표 이동 상하좌우
+    static int[][] map; // 맵 정보 저장
 
-    public static int time, n, size = 2, eat;
-    public static int[][] map, visited;
-    public static Queue<Info> q;
-    public static int[][] dirXY = {{0,1},{0,-1},{1,0},{-1,0}};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 맵 크기
 
-    public static class Info{
-        int x;
-        int y;
-        int move;
-        public Info(int x, int y, int move){
-            this.x = x;
-            this.y = y;
-            this.move = move;
-        }
-    }
+        map = new int[N][N];
+        int[] cur = null; // 현재 상어의 위치
 
-    public static PriorityQueue<Info> pq = new PriorityQueue<>((o1, o2) ->
-    {
-        if(o1.move == o2.move){
-            if(o1.x == o2.x){
-                return o1.y - o2.y;
-            }else{
-                return o1.x - o2.x;
-            }
-        }else{
-            return o1.move - o2.move;
-        }
-    });
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        n = Integer.parseInt(br.readLine());
-
-        map = new int[n][n];
-
-        int[] start = new int[2];
-
-        StringTokenizer st;
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 9){
-                    start[0] = i;
-                    start[1] = j;
-                    map[i][j] = 0;
+        // 맵 정보 입력
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                map[i][j] = sc.nextInt();
+                if (map[i][j] == 9) {
+                    cur = new int[]{i, j}; // 상어의 초기 위치 저장
+                    map[i][j] = 0; // 상어가 있는 위치를 0으로 초기화
                 }
             }
-        }
 
-        while(true){
-            bfs(start);
+        int size = 2; // 상어의 크기
+        int eat = 0; // 먹은 물고기 수
+        int move = 0; // 움직인 총 거리
 
-            if(pq.isEmpty()){
-                break;
+        while (true) {
+            PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) ->
+                    o1[2] != o2[2] ? Integer.compare(o1[2], o2[2]) : (o1[0] != o2[0] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]))
+            );
+            boolean[][] visit = new boolean[N][N];
+
+            que.add(new int[]{cur[0], cur[1], 0}); // 현재 상어의 위치와 이동한 거리를 큐에 추가
+            visit[cur[0]][cur[1]] = true;
+
+            boolean ck = false; // 상어가 먹이를 먹었는지 체크할 변수
+
+            // BFS 탐색
+            while (!que.isEmpty()) {
+                cur = que.poll(); // 큐에서 하나 꺼내옴
+
+                // 먹이가 있으면서 상어의 사이즈보다 작다면?
+                if (map[cur[0]][cur[1]] != 0 && map[cur[0]][cur[1]] < size) {
+                    map[cur[0]][cur[1]] = 0; // 물고기를 제거
+                    eat++;
+                    move += cur[2]; // 움직인 거리를 총 움직인 거리에 추가
+                    ck = true; // 먹이 먹었다고 체크
+                    break;
+                }
+
+                // 상하좌우 탐색
+                for (int k = 0; k < 4; k++) {
+                    int ny = cur[0] + dy[k];
+                    int nx = cur[1] + dx[k];
+
+                    // 범위를 벗어나거나 이미 방문했거나 상어 크기보다 큰 물고기는 무시
+                    if (ny < 0 || nx < 0 || nx >= N || ny >= N || visit[ny][nx] || map[ny][nx] > size)
+                        continue;
+
+                    que.add(new int[]{ny, nx, cur[2] + 1}); // 다음 위치와 이동한 거리를 큐에 추가
+                    visit[ny][nx] = true; // 방문 체크
+                }
             }
-            Info info = pq.poll();
-            time += info.move;
-            map[info.x][info.y] = 0;
-            start[0] = info.x;
-            start[1] = info.y;
-            eat++;
-            if(size == eat){
+
+            // 큐가 비워질 때까지 먹이를 먹은적이 없다면, 더 이상 먹은 물고기가 없으므로 탈출
+            if (!ck)
+                break;
+
+            // 사이즈와 먹이를 먹은 수가 동일하다면 상어의 크기를 증가
+            if (size == eat) {
                 size++;
                 eat = 0;
             }
-            pq.clear();
         }
-        System.out.println(time);
-    }
 
-    public static void bfs(int[] pos){
-        q = new LinkedList<>();
-        q.offer(new Info(pos[0], pos[1], 0));
-
-        visited = new int[n][n];
-
-        while(!q.isEmpty()){
-            Info cur = q.poll();
-
-            for(int i = 0; i < 4; i++){
-                int nx = cur.x + dirXY[i][0];
-                int ny = cur.y + dirXY[i][1];
-
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n){
-                    continue;
-                }
-
-                if(visited[nx][ny] == 0){
-                    if(map[nx][ny] == size || map[nx][ny] == 0){
-                        visited[nx][ny] = 1;
-                        q.offer(new Info(nx,ny,cur.move+1));
-                    }else if(map[nx][ny] < size){
-                        visited[nx][ny] = 1;
-                        pq.offer(new Info(nx,ny,cur.move+1));
-                    }
-                }
-            }
-        }
+        System.out.println(move); // 총 움직인 거리 출력
     }
 }
