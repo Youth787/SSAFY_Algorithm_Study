27bd7abfcf3ef63f5230bb2fa3e5e433f5313b2a
aycho00 <141216710+aycho00@users.noreply.github.com>
2024-02-19 14:07:22 +0900

    Create Solution.java

---
+++ 18_자율문제/2월3주차/조아영/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1+num2;
+    }
+
+    public int solution(int num1, int num2) {
+        return num1-num2;
+    }
+
+    public int solution(int num1, int num2) {
+        return num1*num2;
+    }
+
+    public int solution(int num1, int num2) {
+        return num1/num2;
+    }
+
+    public int solution(int num1, int num2) {
+        int num = (num1*1000)/num2;
+        return num;
+    }
+}
