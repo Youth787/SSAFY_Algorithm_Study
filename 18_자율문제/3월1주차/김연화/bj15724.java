b68c71af29ecc24d9c2b919bafc44985f86dd0d4
myeon0109 <124342362+myeon0109@users.noreply.github.com>
2024-03-06 23:30:53 +0900

    Create bj15724.java

---
+++ 18_자율문제/3월1주차/김연화/bj15724.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    static int [][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N+1][M+1];
+
+        for(int i = 1; i <= N; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= M; ++j)
+            {
+                map[i][j] = Integer.parseInt(st.nextToken()) + map[i-1][j] + map[i][j-1] - map[i-1][j-1];
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i = 0; i < K; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int res = map[x2][y2] - map[x2][y1-1] - map[x1-1][y2] + map[x1-1][y1-1];
+            bw.write(res+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
