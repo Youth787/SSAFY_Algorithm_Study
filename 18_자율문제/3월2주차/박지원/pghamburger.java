cafe0efeea98c5a60d65453beb2397f6631fb7fc
zziony <122343467+zzionyy@users.noreply.github.com>
2024-03-12 08:55:30 +0900

    Update pghamburger.java

--- 18_자율문제/3월2주차/박지원/pghamburger.java
+++ 18_자율문제/3월2주차/박지원/pghamburger.java
@@ -18,3 +18,17 @@ class Solution {
         return answer;
     }
 }
+
+//자바스크립트로 풀었을 때
+function solution(ingredient) {
+    let answer = 0;
+    let stack = [];
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        if (stack.slice(-4).join('') == '1231') {
+            answer++;
+            stack.splice(-4);
+        }
+    }
+    return answer;
+}
