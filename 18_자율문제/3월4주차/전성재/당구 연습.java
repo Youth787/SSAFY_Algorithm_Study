c467b8742f4981a3e7fbd5e0d23dcd91c1bf8b2c
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2024-03-26 22:08:35 +0900

    Create 당구 연습.java

---
+++ 18_자율문제/3월4주차/전성재/당구 연습.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = new int[balls.length];
+
+		for (int i = 0; i < balls.length; i++) {
+			int targetX = balls[i][0];
+			int targetY = balls[i][1];
+
+			int curlen, len = Integer.MAX_VALUE;
+
+			// 좌
+			if (!(startY == targetY && startX >= targetX)) {
+				curlen = getDistance(startX, startY, targetX * (-1), targetY);
+				len = curlen < len ? curlen : len;
+			}
+
+			// 우
+			if (!(startY == targetY && startX <= targetX)) {
+				curlen = getDistance(startX, startY, m + (m - targetX), targetY);
+				len = curlen < len ? curlen : len;
+			}
+
+			// 상
+			if (!(startX == targetX && startY <= targetY)) {
+				curlen = getDistance(startX, startY, targetX, n + (n - targetY));
+				len = curlen < len ? curlen : len;
+			}
+
+			// 하
+			if (!(startX == targetX && startY >= targetY)) {
+				curlen = getDistance(startX, startY, targetX, targetY * (-1));
+				len = curlen < len ? curlen : len;
+			}
+			
+			answer[i] = len;
+		}
+
+		return answer;
+    }
+    
+    public int getDistance(int sx, int sy, int tx, int ty) {
+		return (int) (Math.pow(sx - tx, 2) + Math.pow(sy - ty, 2));
+	}
+}
