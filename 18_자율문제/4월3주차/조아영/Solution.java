d474cc1a432f72f42fcbe0e1a7842a9ea65e7d1e
aycho00 <141216710+aycho00@users.noreply.github.com>
2024-04-13 08:53:22 +0900

    Create Solution.java

---
+++ 18_자율문제/4월3주차/조아영/Solution.java
@@ -0,0 +1,16 @@
+
+class Solution {
+
+    //음양 더하기 
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
