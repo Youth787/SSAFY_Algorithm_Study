8eceee0199a0a9442f09b9b08ddb276255dfe79b
jw-choi-kzz <141205653+jw-choi-kzz@users.noreply.github.com>
2024-04-18 09:52:19 +0900

    Create 행렬의 곱셈.java

---
+++ 18_자율문제/4월4주차/최지원/행렬의 곱셈.java
@@ -0,0 +1,20 @@
+/*행렬 곱이라는 걸 몰랐음*/
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int r1 = arr1.length;
+        int c1 = arr1[0].length;
+        int r2 = arr2.length;
+        int c2 = arr2[0].length;
+        
+        int[][] answer = new int[r1][c2];
+        
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
