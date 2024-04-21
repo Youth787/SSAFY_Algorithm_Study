5404dbfb27bce5c1d94aeb2992555cb506bcd0a9
myeon0109 <124342362+myeon0109@users.noreply.github.com>
2024-04-21 20:19:29 +0900

    Create bj16637.java

---
+++ 18_자율문제/4월4주차/김연화/bj16637.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+
+
+public class Main {
+
+    // 수식의 길이
+    public static int length;
+    // 수식
+    public static char[] expression;
+    // 결과의 최댓값 저장
+    public static int MAX = Integer.MIN_VALUE;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        length = Integer.parseInt(br.readLine());
+        expression = br.readLine().toCharArray();
+
+
+        makeExpression(2, expression[0] - '0');
+        bw.write(String.valueOf(MAX));
+        bw.flush();
+    }
+
+    public static void makeExpression(int now,int total) {
+
+        // 종료 조건
+        if(now>=length){
+            MAX = Math.max(MAX, total);
+            return;
+        }
+
+        // 괄호를 사용하지 않음 A+B
+        makeExpression(now + 2, calculate(total, expression[now] - '0', expression[now - 1]));
+
+
+        // now부터 시작하는 괄호를 사용함. A+(B+C)
+        if(now+2<length){
+            int sum = calculate(expression[now] - '0', expression[now + 2] - '0', expression[now + 1]);
+            int sumTotal = calculate(total, sum, expression[now - 1]);
+            makeExpression(now + 4, sumTotal);
+
+        }
+
+    }
+
+    // 계산 결과 반환
+    public static int calculate(int sum, int plus, char sep) {
+        if(sep=='+')
+            return sum + plus;
+        if(sep=='-')
+            return sum - plus;
+        return sum * plus;
+    }
+
+
+}
