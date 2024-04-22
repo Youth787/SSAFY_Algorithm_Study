d7e8c4c938f789f224c2c7794c9935c4b332c2f8
SeRi <141606477+onid057@users.noreply.github.com>
2024-04-22 18:46:38 +0900

    Create 괄호 회전하기.java

---
+++ 18_자율문제/4월4주차/김세리/괄호 회전하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            String a = s.substring(i)+s.substring(0,i);
+            if(isValid(a)){
+                answer++;
+            }
+        }
+        return answer;
+    }//main
+    
+    private boolean isValid(String a){
+        Stack<Character> stack = new Stack<>();
+        for(char c: a.toCharArray()){
+            if(c =='('||c=='{'||c=='[') stack.push(c);
+            else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((top=='('&&c!=')')||(top=='{'&&c!='}')||(top=='['&&c!=']')) return false;
+            }
+        }
+        // stack 비어있지 않으면 자동으로 false return된다
+        return stack.isEmpty();
+    }//isValid
+}
