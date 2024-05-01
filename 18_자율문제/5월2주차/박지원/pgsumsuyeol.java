59848f9f7684994cbbad0dd5b39df1829bf20844
zziony <122343467+zzionyy@users.noreply.github.com>
2024-05-02 08:52:09 +0900

    Update pgsumsuyeol.java

--- 18_자율문제/5월2주차/박지원/pgsumsuyeol.java
+++ 18_자율문제/5월2주차/박지원/pgsumsuyeol.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-//프로그래머스 연속 수열 합의 갯수
+//프로그래머스 연속 부분 수열 합의 갯수
 //set 써서 중복없이 갯수구했다. 배열이 돌아간다그래서 두개 합친 배열을 만들었는데,, 다른사람 풀이 보니 그럴필요 없이 i를 적당 값으로 나눠주면 될 일이었다. 하하
 class Solution {
     public int solution(int[] elements) {
