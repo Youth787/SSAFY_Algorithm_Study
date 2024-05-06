a22da5d6688da419a735752c66b8546ca06f0453
Youth787 <wkdus885@naver.com>
2024-05-07 00:25:18 +0900

    Create 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql

---
+++ 18_자율문제/5월2주차/정연미/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT
+    e.emp_no,
+    e.emp_name,
+    CASE 
+        WHEN AVG(score) >= 96 THEN 'S'
+        WHEN AVG(score) >= 90 THEN 'A'
+        WHEN AVG(score) >= 80 THEN 'B'
+        ELSE 'C'
+    END AS grade,
+    CASE 
+        WHEN AVG(score) >= 96 THEN e.sal * 0.2
+        WHEN AVG(score) >= 90 THEN e.sal * 0.15
+        WHEN AVG(score) >= 80 THEN e.sal * 0.1
+        ELSE e.sal * 0
+    END AS bonus
+FROM hr_employees e
+JOIN hr_grade g
+ON e.emp_no = g.emp_no
+GROUP BY e.emp_no
+ORDER BY e.emp_no
