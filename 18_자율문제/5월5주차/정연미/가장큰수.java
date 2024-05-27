8faeb83d0bde19b1402caf29ad3a1d1250f16da4
Youth787 <wkdus885@naver.com>
2024-05-27 09:22:46 +0900

    Update 가장큰수.java

--- 18_자율문제/5월5주차/정연미/가장큰수.java
+++ 18_자율문제/5월5주차/정연미/가장큰수.java
@@ -1,4 +1,34 @@
+// int 배열을 list 로 변환해주는 이유는 Integer 자료형을 담기 위해서이다. 
+// Collections.sort(list, (a,b)->{});
+// Integer.parseInt() -> 문자열 정수값으로 변환 
+// Integer.compare : 두 정수값 비교 
+// return 뒤의 -값은 내림차순 의미 
 
+
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<numbers.length; i++){
+            list.add(numbers[i]);
+        }
+        Collections.sort(list, (a,b)->{
+            String aa = String.valueOf(a);
+            String bb = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(aa+bb),Integer.parseInt(bb+aa));
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<list.size(); i++){
+            sb.append(list.get(i));
+        }
+        answer = sb.toString();
+        if(answer.charAt(0)=='0') answer = "0";
+        return answer;
+    }
+}
 
 // 시간초과 발생 
 import java.util.*;
