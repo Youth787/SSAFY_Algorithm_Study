1cdec9d799ec1bc8259bed5904c21cf0131c4f5f
myeon0109 <124342362+myeon0109@users.noreply.github.com>
2024-06-11 17:38:55 +0900

    Create bj1764.java

---
+++ 18_자율문제/6월2주차/김연화/bj1764.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        HashSet<String> set = new HashSet<>(); 
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String tmp = br.readLine();
+            if(set.contains(tmp)){
+                result.add(tmp);
+            }
+        }
+
+        Collections.sort(result);
+
+        // print result
+        System.out.println(result.size());
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
