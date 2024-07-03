d607fc63c5dd5fdf9e765845d5fbf6230922481f
Youth787 <wkdus885@naver.com>
2024-07-03 18:45:12 +0900

    Update SQL.sql

--- 18_자율문제/7월2주차/정연미/SQL.sql
+++ 18_자율문제/7월2주차/정연미/SQL.sql
@@ -29,3 +29,13 @@ concat
 */
 ---------------------------------------
 
+https://school.programmers.co.kr/learn/courses/30/lessons/59410
+# NULL 처리하기
+
+SELECT ANIMAL_TYPE,
+CASE WHEN NAME IS NULL THEN 'No name' 
+ELSE NAME END AS NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+/* IS NULL */
