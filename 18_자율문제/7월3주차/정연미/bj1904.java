0628245dfb806d4d8ff29ae46555fbf8b552e560
Youth787 <wkdus885@naver.com>
2024-07-15 15:02:43 +0900

    Create bj1904.java

---
+++ 18_자율문제/7월3주차/정연미/bj1904.java
@@ -0,0 +1,17 @@
+package SILVER;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj1904 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N+1];
+        dp[0] =1;
+        dp[1] = 1;
+        for(int i=2; i<=N; i++) dp[i] = (dp[i-1] + dp[i-2])%15746;
+        System.out.println(dp[N]%15746);
+    }
+}
