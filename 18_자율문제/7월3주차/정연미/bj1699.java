18c5bd3aef39f01b4cf598ddee90074c60a69778
Youth787 <wkdus885@naver.com>
2024-07-15 16:43:13 +0900

    Update bj1699.java

--- 18_자율문제/7월3주차/정연미/bj1699.java
+++ 18_자율문제/7월3주차/정연미/bj1699.java
@@ -5,22 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class bj1699 {
+    static int result =0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int result = 0;
         int[] dp = new int[N+1];
-        dp[1] = 1;
-        for(int i=2; i<=N; i++){
-            result =Integer.MAX_VALUE;
-            for(int j=1; j<=(int)i/2; j++){
-                if(j*j ==i){
-                    result = 1;
-                    break;
-                }else result = Math.min(result, dp[j]+dp[i-j]);
-            }
-            dp[i] = result;
+
+        for(int i=1; i<=N; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++)
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
         }
-        System.out.println(result);
+        System.out.println(dp[N]);
     }
 }
