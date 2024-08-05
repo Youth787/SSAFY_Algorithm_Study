c56feefedab04f24fb65c7671f898230532f8a89
myeon0109 <124342362+myeon0109@users.noreply.github.com>
2024-08-06 07:11:23 +0900

    Create bj12851.java

---
+++ 18_자율문제/8월1주차/김연화/bj12851.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int N, K;
+    static int[] time = new int[100001];
+    static int minTime = 987654321;
+    static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        if (N >= K) {
+            System.out.println((N-K) + "\n1");
+            return;
+        }
+
+        bfs();
+
+        System.out.println(minTime + "\n" + count);
+    }
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        q.add(N);
+        time[N] = 1;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            // now 방문 시간이 최소 시간보다 크면 뒤는 더 볼 필요 없음
+            if (minTime < time[now]) return;
+
+            for (int i=0; i<3; i++) {
+                int next;
+
+                if (i == 0)         next = now + 1;
+                else if (i == 1)    next = now - 1;
+                else                next = now * 2;
+
+                if (next < 0 || next > 100000) continue;
+
+                if (next == K) {
+                    minTime = time[now];
+                    count++;
+                }
+
+                // 첫 방문이거나 (time[next] == 0)
+                // 이미 방문한 곳이어도 같은 시간에 방문했다면 (time[next] == time[now] + 1)
+                // 경우의 수에 추가될 수 있기 때문에 Queue 에 한번 더 넣어줌
+                if (time[next] == 0 || time[next] == time[now] + 1) {
+                    q.add(next);
+                    time[next] = time[now] + 1;
+                }
+            }
+        }
+    }
+}
