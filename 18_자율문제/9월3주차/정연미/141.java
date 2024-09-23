bb3d88cbd7f3c3b88c7a6520486a84520aa63b45
Youth787 <wkdus885@naver.com>
2024-09-23 19:58:05 +0900

    Create 141.java

---
+++ 18_자율문제/9월3주차/정연미/141.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        if(head==null) return false;
+
+        while(fast!=null &&fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast ==slow) return true;
+        }
+        return false;
+    }
+}
