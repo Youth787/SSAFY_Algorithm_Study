09e5186ed030069453f1ec3963b84ce1b922b223
Youth787 <wkdus885@naver.com>
2024-10-18 19:11:47 +0900

    Update bj20210.java

--- 18_자율문제/2024_하반기/10월3주차/정연미/bj20210.java
+++ 18_자율문제/2024_하반기/10월3주차/정연미/bj20210.java
@@ -11,7 +11,7 @@ import java.util.*;
 // toLowerCase // toUpperCase
 
 public class bj20210 {
-    public static void main(String[] args ) throws IOException {
+   public static void main(String[] args ) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<String> list = new ArrayList<>();
         int N = Integer.parseInt(br.readLine());
@@ -21,60 +21,71 @@ public class bj20210 {
             list.add(input);
         }
 
-        Collections.sort(list,(a,b)->compareCustom(a,b));
+        Collections.sort(list,(a, b)->compareCustom(a,b));
         for(String pick : list) System.out.println(pick);
     }
+
     public static int compareCustom(String a, String b){
-        int i =0; int j =0;
-        while(i<a.length()&&j<b.length()){
+        int i=0; int j =0;
+        while(i<a.length()&& j<b.length()){
             char charA = a.charAt(i);
             char charB = b.charAt(j);
 
-            //둘다 숫자인경우
+            // 둘다 숫자일 경우
             if(Character.isDigit(charA)&&Character.isDigit(charB)){
-                String numA = extractNum(a,i);
-                String numB = extractNum(b,j);
-                int numcompare = compareNum(numA,numB);
-                if(numcompare!=0) return numcompare;
+                String numA = extractNum(a, i);
+                String numB = extractNum(b, j);
+
+                int leadingZerosA = countLeadingZeros(numA);
+                int leadingZerosB = countLeadingZeros(numB);
+
+                int compare = compareNum(numA.substring(leadingZerosA), numB.substring(leadingZerosB));
+                if(compare!=0) return compare;
                 if(numA.length()!=numB.length()) return numA.length()-numB.length();
-                // 동일한 문자열이라면?
                 i+=numA.length();
                 j+=numB.length();
             }
-            //둘다 문자인경우
+
+            // 둘다 문자일 경우
             else if(Character.isLetter(charA)&&Character.isLetter(charB)){
-                int charcompare = compareLetter(charA, charB);
-                if(charcompare!=0) return charcompare;
+                int compareletter = compareLetter(charA, charB);
+                if(compareletter!=0) return compareletter;
                 i++;
                 j++;
             }
-            // 자료형이 다른경우 숫자가 먼저온다.
+            // 서로 자료형 다를 경우
             else return Character.isDigit(charA)?-1:1;
-            // 문자열이 모두 끝났는가?
         }
-        return a.length()-b.length();
+        return a.length() - b.length();
     }
-
-    public static String extractNum(String s, int index){
-        StringBuilder sb = new StringBuilder();
-        while(index<s.length()&&Character.isDigit(s.charAt(index))){
-            sb.append(s.charAt(index));
-            index++;
+    public static String extractNum(String s , int idx){
+        StringBuilder num = new StringBuilder();
+        while(idx<s.length()&& Character.isDigit(s.charAt(idx))){
+            num.append(s.charAt(idx));
+            idx++;
         }
-        return sb.toString();
+        return num.toString();
+    }
+    public static int compareNum(String a, String b) {
+        // 숫자 문자열의 길이가 다르면 길이로 우선 비교
+        if (a.length() != b.length()) {
+            return a.length() - b.length();
+        }
+        // 길이가 같으면 문자열을 그대로 비교 (숫자 값으로 비교하는 효과를 줌)
+        return a.compareTo(b);
     }
 
-    public static int compareNum(String a , String b){
-        // 자바에서 형변환 할 수 있는 함수는 parse와 valueOf가 있다.
-        long valA = Long.parseLong(a);
-        long valB = Long.valueOf(b);
-        return Long.compare(valA,valB);
+    public static int countLeadingZeros(String s) {
+        int count = 0;
+        while (count < s.length() && s.charAt(count) == '0') {
+            count++;
+        }
+        return count;
     }
-
-    public static int compareLetter(Character a, Character b){
-        char lowerA = Character.toLowerCase(a);
-        char lowerB = Character.toLowerCase(b);
-        if(lowerA!=lowerB) return lowerA-lowerB;
+    public static int compareLetter(char a , char b){
+        int A = Character.toLowerCase(a);
+        int B = Character.toLowerCase(b);
+        if(A!=B) return A-B;
         return a-b;
     }
 }
