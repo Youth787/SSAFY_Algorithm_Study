0e2311c363abd46a1ca870b43a38a09a487fa99d
Youth787 <wkdus885@naver.com>
2024-11-25 23:06:50 +0900

    Update bj1379.java

--- 18_자율문제/2024_하반기/11월4주차/정연미/bj1379.java
+++ 18_자율문제/2024_하반기/11월4주차/정연미/bj1379.java
@@ -1,90 +1,71 @@
-// https://velog.io/@shinjy9802/%EB%B0%B1%EC%A4%80-1379-%EA%B0%95%EC%9D%98%EC%8B%A4-2-Java
+package GOLD;
 
-class Lecture {
-    int num, roomNum, start, end;
-    Lecture(int num, int start, int end) {
-        this.num = num;
-        this.roomNum = -1;
-        this.start = start;
-        this.end = end;
-    }
-    
-    public void setRoomNum(int roomNum) {
-        this.roomNum = roomNum;
-    }
-    
-    public int getRoomNum() {
-        return this.roomNum;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class lecture {
+    int num; int startT; int endT;
+
+    public lecture(int num, int startT, int endT){
+        this.num=num;
+        this.startT=startT;
+        this.endT=endT;
     }
 }
 
-public class Main {
-    static int N;
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        Lecture[] lectures = new Lecture[N];
-        for(int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            lectures[i] = new Lecture(num, start, end);
-        }
-        Arrays.sort(lectures, new Comparator<Lecture>() {
-            @Override
-            public int compare(Lecture l1, Lecture l2) {
-                return Integer.compare(l1.num, l2.num);
-            }
-        });
-        StringBuilder sb = new StringBuilder();
-        sb.append(assignRoomNum(lectures)).append('\n');
-        for(int i=0; i<N; i++) {
-            sb.append(lectures[i].getRoomNum()).append('\n');
-        }
-        System.out.println(sb.toString().trim());
+class room{
+    int roomnum, end;
+
+    public room(int roomnum, int end){
+        this.roomnum = roomnum;
+        this.end = end;
     }
-    
-    static int assignRoomNum(Lecture[] lectures) {
-        ArrayList<Lecture> sList = new ArrayList<>();
-        ArrayList<Lecture> eList = new ArrayList<>();
-        for(int i=0; i<lectures.length; i++) {
-            sList.add(lectures[i]);
-            eList.add(lectures[i]);
+}
+
+public class bj1379 {
+    public static void main(String[] args) throws IOException{
+        PriorityQueue<lecture> pq = new PriorityQueue<>((a,b)-> {
+            if(a.startT==b.startT) return a.endT-b.endT;
+            return a.startT-b.startT;
+        });
+
+        PriorityQueue<room> roompq = new PriorityQueue<>((a,b)->(a.end-b.end));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] result = new int[N+1];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int number = Integer.parseInt(st.nextToken());
+            int startT = Integer.parseInt(st.nextToken());
+            int endT = Integer.parseInt(st.nextToken());
+            pq.add(new lecture(number,startT,endT));
         }
-        
-        Collections.sort(sList, new Comparator<Lecture>() {
-            @Override
-            public int compare(Lecture l1, Lecture l2) {
-                return Integer.compare(l1.start, l2.start);
-            }
-        });
-        
-        Collections.sort(eList, new Comparator<Lecture>() {
-            @Override
-            public int compare(Lecture l1, Lecture l2) {
-                return Integer.compare(l1.end, l2.end);
-            }
-        });
-        
-        int k = 1; 
-        int sp = 0;
-        for(int i=0; i<eList.size(); i++) {
-            if(sp == sList.size()) {
-                break;
-            }
-            while(sp < sList.size()) {
-                if(eList.get(i).end > sList.get(sp).start) {
-                    sList.get(sp).setRoomNum(k);
-                    k += 1;
-                    sp += 1;
-                } else {
-                    sList.get(sp).setRoomNum(eList.get(i).getRoomNum());
-                    sp += 1;
-                    break;
+
+        while(!pq.isEmpty()){
+            lecture curr = pq.poll();
+            if(roompq.isEmpty()){
+                result[curr.num] = 1;
+                roompq.add(new room(1,curr.endT));
+            }else{
+                if(roompq.peek().end<=curr.startT){
+                    result[curr.num] = roompq.peek().roomnum;
+                    roompq.poll();
+                    roompq.add(new room(result[curr.num],curr.endT));
+                }else{
+                    result[curr.num] = roompq.size()+1;
+                    roompq.add(new room(result[curr.num],curr.endT));
                 }
             }
         }
-        return k -= 1;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(roompq.size()).append("\n");
+        for(int i=1; i<=N; i++){
+            sb.append(result[i]).append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
