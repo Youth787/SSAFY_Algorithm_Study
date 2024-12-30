869f025432fca20581cf4786262f88075aa1e97d
icandoit <122343467+fourtwofive@users.noreply.github.com>
2024-12-30 16:42:56 +0900

    Create pgpricestock.java

---
+++ 18_자율문제/2024_하반기/12월5주차/박지원/pgpricestock.java
@@ -0,0 +1,24 @@
+//프로그래머스 주식가격 자바
+//stack
+
+import java.util.Stack;
+class Solution {
+	public int[] solution(int[] prices) {
+		int[] answer = new int[prices.length];
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < prices.length; i++) {
+			while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+				answer[stack.peek()] = i - stack.peek();
+				stack.pop();
+			}
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty()) {
+			answer[stack.peek()] = prices.length - stack.peek() - 1;
+			stack.pop();
+		}
+		return answer;
+	}
+}
