290501ae110a3561f1b239a42d2f0bbf069d8ba3
icandoit <122343467+fourtwofive@users.noreply.github.com>
2025-01-12 21:17:36 +0900

    Create pgskilltree.java

---
+++ 18_자율문제/2025_상반기/1월1주차/박지원/pgskilltree.java
@@ -0,0 +1,17 @@
+//프로그래머스 스킬트리 자바
+//문자열, replaceAll, substring
+
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for (int i = 0; i < skill_trees.leng aceAll("[^" + skill + "]", "");
+            String need = skill.substring(0, pre.length());
+            if(pre.equals(need)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
