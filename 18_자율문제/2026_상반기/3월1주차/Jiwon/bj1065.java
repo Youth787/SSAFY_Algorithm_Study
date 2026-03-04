df26880d8a8a681a2d5721fd4d23a666045b1040
zziony <wldnjs1123@gmail.com>
2026-03-04 15:24:15 +0900

    modify folder

--- 18_자율문제/2026_상반기/3월1주차/Jiwon/bj1065.java
+++ 18_자율문제/2026_상반기/3월1주차/Jiwon/bj1065.java
@@ -1,23 +1,24 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class bj1065 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         if (n <= 99) {
-            return n;
+            System.out.println(n);
+            return;
         }
         int answer = 99;
         int first, second, third;
-        for (int i = 100; i <= n; i++) {
-            first = i % 100;
+        for (int i = 100; i <= Math.min(n, 999); i++) {
+            first = i % 10;
             second = (i / 10) % 10;
             third = (i / 100) % 10;
             if ((third - second) == (second - first)) answer++;
         }
 
-        return answer;
+        System.out.println(answer);
 
 
     }
